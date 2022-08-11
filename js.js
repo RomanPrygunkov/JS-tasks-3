1366e3e98fce02b24b463962f668764101bb80d2
RomanPrygunkov <103605350+RomanPrygunkov@users.noreply.github.com>
2022-08-11 19:05:49 +0300

    added %

--- js.js
+++ js.js
@@ -1,22 +1,17 @@
-function getDivisorsCount (number) {
-    let division = 0  
+function getDivisorsCount(number) {
     let count = 0
 
     if (typeof number !== "number") {
         count = NaN
     } else if (number < 0 || !Number.isInteger(number)) {
-        alert ("number должен быть целым числом и больше нуля!")
+        alert("number должен быть целым числом и больше нуля!")
     } else {
+        for (let i = 1; i <= number; i += 1)
+            if (number % i === 0) {
+                count += 1;
+            }
+    }
+     return console.log(count);   
+}   
 
-    for (let i = 1; i <= number; i += 1) {
-        division = number / i
-       
-        if (Number.isInteger(division)) {
-            count +=1;
-        }
-        }
-        }
-    return console.log(count);
-}
-
-getDivisorsCount(12)
+getDivisorsCount(30)
